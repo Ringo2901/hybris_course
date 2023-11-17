3c6380f79d146f227248b48564fe53b6c2d9d776
Ringo2901 <nekitale@yandex.ru>
2023-11-17 23:24:55 +0300

    Displaying a User: Looking Up and Displaying a Custom User

--- bin/custom/concerttours/web/src/concerttours/controller/UserController.java
+++ bin/custom/concerttours/web/src/concerttours/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import de.hybris.platform.core.model.user.UserModel;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -14,8 +15,16 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public String handleRequest(final ModelMap model) throws Exception {
-        UserModel user = userService.getCurrentUser();
+    public String handleRequest(final ModelMap model, @RequestParam String uid) throws Exception {
+        UserModel user = null;
+        if (uid == null || uid.equals(""))
+        {
+            user = userService.getCurrentUser();
+        }
+        else
+        {
+            user = userService.getUser(uid);
+        }
         model.put("user", user);
         return "user";
     }
