34a036bfcb7b534befe02510d43db8fd2812de42
Ringo2901 <nekitale@yandex.ru>
2023-11-17 23:08:23 +0300

    Displaying a User:Injecting the UserService

--- bin/custom/concerttours/web/src/concerttours/controller/UserController.java
+++ bin/custom/concerttours/web/src/concerttours/controller/UserController.java
@@ -1,5 +1,7 @@
 package concerttours.controller;
 
+import de.hybris.platform.servicelayer.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public String handleRequest(final ModelMap model) throws Exception {
