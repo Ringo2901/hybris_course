aa09a4f4d299768a5e9054e0754150eab0b9c47a
Ringo2901 <nekitale@yandex.ru>
2023-11-20 19:08:52 +0300

    Add facade which converts the model from the previous task to a data

--- bin/custom/concerttours/src/concerttours/service/impl/DefaultContactRequestService.java
+++ bin/custom/concerttours/src/concerttours/service/impl/DefaultContactRequestService.java
@@ -4,35 +4,25 @@ import concerttours.daos.ContactRequestDao;
 import concerttours.service.ContactRequestService;
 import de.hybris.platform.servicelayer.exceptions.AmbiguousIdentifierException;
 import de.hybris.platform.servicelayer.exceptions.UnknownIdentifierException;
-import de.hybris.platform.servicelayer.search.FlexibleSearchQuery;
-import de.hybris.platform.servicelayer.search.FlexibleSearchService;
-import de.hybris.platform.servicelayer.search.SearchResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.training.model.ContactRequestModel;
 
 import java.util.List;
 
 @Component
-public class DefaultContactRequestService implements ContactRequestService
-{
+public class DefaultContactRequestService implements ContactRequestService {
     @Autowired
     private ContactRequestDao contactRequestDao;
 
-    public ContactRequestModel getContactRequest(final String sender)
-    {
+    public ContactRequestModel getContactRequest(final String sender) {
         final List<ContactRequestModel> result = contactRequestDao.findBySender(sender);
         final int resultCount = result.size();
-        if (resultCount == 0)
-        {
+        if (resultCount == 0) {
             throw new UnknownIdentifierException(
                     "ContactRequest with sender '" + sender + "' not found!"
             );
-        }
-        else if (resultCount > 1)
-        {
+        } else if (resultCount > 1) {
             throw new AmbiguousIdentifierException(
                     "ContactRequest with sender '" + sender + "' is not unique, "
                             + resultCount + " requests found!"
